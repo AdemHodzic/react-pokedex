48e05dda10e4a2f0542b132672ba859b5c95e0dd
Hodzic Adem <athos.blade@gmail.com>
2019-03-01 19:50:03 +0100

    Updates

---
+++ precache-manifest.a8bf02db5f74f597527d6aa10ed6023b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a8f6d423df5904d5a47",
+    "url": "/pokedex/static/css/main.7487773f.chunk.css"
+  },
+  {
+    "revision": "7a8f6d423df5904d5a47",
+    "url": "/pokedex/static/js/main.7a8f6d42.chunk.js"
+  },
+  {
+    "revision": "c92f8f4bce56a57764ce",
+    "url": "/pokedex/static/js/runtime~main.c92f8f4b.js"
+  },
+  {
+    "revision": "0e1d97215408d0b56641",
+    "url": "/pokedex/static/js/2.0e1d9721.chunk.js"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokedex/static/media/pokemon-font.36d26dbc.ttf"
+  },
+  {
+    "revision": "674fe440f4bf66d8b86d409318676ab7",
+    "url": "/pokedex/static/media/working_pikachu.674fe440.gif"
+  },
+  {
+    "revision": "e8ddb9b81f747b7c3966df32b2dc8654",
+    "url": "/pokedex/static/media/sad_pikachu.e8ddb9b8.webp"
+  },
+  {
+    "revision": "8354c2d18b90a9b368ada1d5d52f2df8",
+    "url": "/pokedex/index.html"
+  }
+];
